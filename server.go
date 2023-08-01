af333be92ef0a24a3a29ff4a9d1a9fd9f64a10f9
anjankow <anjankow@protonmail.com>
2023-08-01 20:11:22 +0200

    add simple endpoints

--- server.go
+++ server.go
@@ -6,10 +6,26 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+func getFish(c echo.Context) error {
+	query := c.Request().URL.Query()
+	if weird := query.Get("weird"); weird == "true" {
+		// weird fish requested, there you go!
+		return c.String(http.StatusOK, "barreleye, just google it")
+	}
+
+	return c.String(http.StatusOK, "herrings and sardines")
+}
+
+func getBirds(c echo.Context) error {
+	return c.String(http.StatusOK, "owls (are not what they seem)")
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+
+	// routing
+	e.GET("/api/animals/fish", getFish)
+	e.GET("/api/animals/birds", getBirds)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
